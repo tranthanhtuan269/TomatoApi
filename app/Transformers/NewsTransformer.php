7b6e9dbd99a341c7dfdbf7f5db7917655909213c
Tuan TT <tuantt@tohsoft.com>
2018-10-13 17:09:07 +0700

    css

--- app/Transformers/NewsTransformer.php
+++ app/Transformers/NewsTransformer.php
@@ -18,8 +18,8 @@ class NewsTransformer extends TransformerAbstract
             'title' => $news->title,
             'content' => $news->content,
             'author' => $news->author,
-            'created_at' => $news->created_at,
-            'updated_at' => $news->updated_at
+            'created_at' => $news->created_at->format('d-m-y'),
+            'updated_at' => $news->updated_at->format('d-m-y')
         ];
     }
 }
