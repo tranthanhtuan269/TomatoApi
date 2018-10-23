a064aa6473f8f3aea4670043fb4772d684e7c4af
Tuan TT <tuantt@tohsoft.com>
2018-10-23 21:42:46 +0700

    add api upload file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -130,41 +130,30 @@ class HomeController extends Controller
     }
 
     public function uploadImageApi(Request $request){
-        // Path to move uploaded files
-        $target_path = base_path('public/images/');
-         
-        // array for final json respone
-        $response = array();         
-         
-        if (isset($_FILES['image']['name'])) {
-            $target_path = $target_path . basename($_FILES['image']['name']);
-         
-            $response['file_name'] = basename($_FILES['image']['name']);
-         
-            try {
-                // Throws exception incase file is not being moved
-                if (!move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
-                    // make error flag true
-                    $response['error'] = true;
-                    $response['message'] = 'Could not move the file!';
+        $part = base_path('public/images/');
+        $filename = rand(9,9999).basename($_FILES["imageupload"]["name"]);
+        $res = array(); 
+        $kode = ""; 
+        $pesan = ""; 
+
+        if($_SERVER['REQUEST_METHOD'] == "POST")
+        {
+            if($_FILES['imageupload'])
+            {
+                $destinationfile = $part.$filename;
+                if(move_uploaded_file($_FILES['imageupload']['tmp_name'], $destinationfile))
+                {
+                    return \Response::json(array('code' => '200', 'message' => 'success', 'image_url' => $destinationfile));
+                }else
+                {
+                    return \Response::json(array('code' => '403', 'message' => 'unsuccess', 'image_url' => ""));
                 }
-         
-                // File successfully uploaded
-                $response['message'] = 'File uploaded successfully!';
-                $response['error'] = false;
-                $response['file_path'] = $target_path . basename($_FILES['image']['name']);
-            } catch (Exception $e) {
-                // Exception occurred. Make error flag true
-                $response['error'] = true;
-                $response['message'] = $e->getMessage();
+            }else{
+                return \Response::json(array('code' => '404', 'message' => 'unsuccess', 'image_url' => ""));
             }
-        } else {
-            // File parameter is missing
-            $response['error'] = true;
-            $response['message'] = 'Not received any file!';
+        }else
+        {
+            return \Response::json(array('code' => '404', 'message' => 'unsuccess', 'image_url' => ""));
         }
-         
-        // Echo final json response to client
-        echo json_encode($response);
     }
 }
