c04509e9de4cb19ad4f2820ed59bc06ad12146f7
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2018-10-28 01:12:51 +0700

    css

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -19,12 +19,12 @@
         	    ?>
         	    <div class="row order-row">
                     <div class="title-order">ORD{{ date("Ymd") }}{{ $order->id }}</div>
-                    <div class="starttime">Thời gian bắt đầu: {{ date('H:i:s d-m-Y', intval($order->start_time) / 1000) }}</div>
-                    <div class="username">Họ và Tên: {{ $order->username }}</div>
-                    <div class="userphone">Số điện thoại: +{{ $order->user->phone }}</div>
-                    <div class="address">Địa chỉ: {{ $order->number_address }} - {{ $order->address }}</div>
-                    <div class="state">Trạng thái: @if($order->state == 0) Chưa duyệt @elseif($order->state == 1) Đã duyệt @else Đã hủy @endif</div>
-                    <div class="promotion_code">Mã giảm giá: @if(isset($order->promotion_code)) {{ $order->promotion_code }} @else Không có @endif</div>
+                    <div class="starttime">Thời gian bắt đầu: <b><i>{{ date('H:i:s d-m-Y', intval($order->start_time) / 1000) }}</i></b></div>
+                    <div class="username">Họ và Tên: <b><i>{{ $order->username }}</i></b></div>
+                    <div class="userphone">Số điện thoại: <b><i>+{{ $order->user->phone }}</i></b></div>
+                    <div class="address">Địa chỉ: <b><i>{{ $order->number_address }} - {{ $order->address }}</i></b></div>
+                    <div class="state">Trạng thái: <b><i>@if($order->state == 0) Chưa duyệt @elseif($order->state == 1) Đã duyệt @else Đã hủy @endif</i></b></div>
+                    <div class="promotion_code">Mã giảm giá: <b><i>@if(isset($order->promotion_code)) {{ $order->promotion_code }} @else Không có @endif</i></b></div>
                     <div class="list_packages">{{ $order->list_packages }}</div>
 
                     <div class="accept-remove">
