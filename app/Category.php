9118b4f5e27c9a26d7b3da2b4c81f02e8515c49c
tuantt <tuantt@tohsoft.com>
2019-10-17 10:45:22 +0700

    css

--- app/Category.php
+++ app/Category.php
@@ -22,6 +22,6 @@ class Category extends Model
     }
 
     public function products(){
-        return $this->hasMany('App\Product');
+        return $this->hasMany('App\Product')->where('active', 1);
     }
 }
